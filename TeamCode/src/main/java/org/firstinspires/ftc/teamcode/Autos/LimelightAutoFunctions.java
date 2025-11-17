33f5f8915794e39519384fcacfe087431f9c8eb4
AlexIsHere7 <106027707+AlexIsHere7@users.noreply.github.com>
2025-11-17 17:32:18 -0600

    Limelight With Drivetrain Macros

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autos/LimelightAutoFunctions.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode.Autos;
+
+import com.qualcomm.hardware.limelightvision.Limelight3A;
+
+public class LimelightAutoFunctions {
+    private static Limelight3A Lemon;
+
+    public static void LimeObelisk() {
+
+
+    }
+}
